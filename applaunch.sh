8ed25017f3dba9be99aec536036b007e9b4b6cb8
Arnolxu <cinaryilmaz.gnu@gmail.com>
2022-09-10 00:56:41 +0300

    more stuff

--- applaunch.sh
+++ applaunch.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-app=`echo -e "Terminal\nFirefox\nSpotify\nVSCode" | dmenu -l 10 -i -p "Select application to launch:"`
+app=`echo -e "Terminal\nFirefox\nSpotify\nVSCode\nTor Browser\nA website" | dmenu -l 10 -i -p "Select application to launch:"`
 case $app in
   Terminal)
     st
@@ -12,6 +12,32 @@ case $app in
     ;;
   VSCode)
     code
+    ;;
+  Tor\ Browser)
+    tor-browser # or start-tor-browser idk i name my executables myself
+    ;;
+  A\ website)
+    website=`echo -e "Youtube\nGithub\nQoLang\nQoLang website\nBack" | dmenu -l 10 -i -p "Select a website to open:"`
+    case $website in
+      Youtube)
+        firefox https://youtube.com
+        ;;
+      Github)
+        firefox https://github.com
+        ;;
+      QoLang)
+        firefox https://github.com/QoLang/QoLang
+        ;;
+      QoLang\ website)
+        firefox https://qolang.camroku.tech
+        ;;
+      Back)
+        applaunch
+        ;;
+      *)
+        exit 0
+        ;;
+    esac
     ;;
   "")
     exit 0
