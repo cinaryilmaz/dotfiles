2999e67df63dd318fae7234cede8ba303bb6c650
Arnolxu <cinaryilmaz.gnu@gmail.com>
2022-09-10 01:29:08 +0300

    oh no :sob:

--- applaunch.sh
+++ applaunch.sh
@@ -1,3 +1,3 @@
+#!/bin/bash
 app=`ls ~/.applsh | dmenu -l 10 -i -p "Select application to launch:"`
-
 ls ~/.applsh | grep -F -q -x "$app" && sh -c "`cat ~/.applsh/\"$app\"`" || st -e sh -c "echo Invalid application && sleep 3"
